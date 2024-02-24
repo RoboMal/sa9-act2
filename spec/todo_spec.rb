f0972096cc9e72927f462687329bdb6cd41dab0e
Malcolm King <malcolm.elijah.king@gmail.com>
2024-02-23 19:50:54 -0600

    Added solution to problem 2

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -2,22 +2,26 @@
 require 'todo'
 
 RSpec.describe TodoList do
-
+  errands = TodoList.new()
     describe "#add" do
       it "adds a todo to the list" do
-        # Fill in the test
+        errands.add('Buy milk')
+        errands.add('Walk dog')
+        errands.add('Finish homework')
+        expect(errands.todos). to eq ['Buy milk', 'Walk dog', 'Finish homework']
       end
     end
 
     describe "#remove" do
       it "removes a todo from the list" do
-        # Fill in the test
+        errands.remove('Finish homework')
+        expect(errands.todos). to eq ['Buy milk', 'Walk dog']
       end
     end
 
     describe "#todos" do
       it "returns all todos" do
-        # Fill in the test
+        expect(errands.todos). to eq errands.todos
       end
     end
   end
